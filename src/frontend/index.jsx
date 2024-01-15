4955dca34cf629c77c516e6c1329bbb47b1bfc49
Knut Sveidqvist <knsv@sveido.com>
2024-01-15 17:36:34 +0100

    Some updates

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,18 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import React, { useEffect, useState } from "react";
+import ForgeReconciler, { Text } from "@forge/react";
+import { invoke } from "@forge/bridge";
+import { ImageProps } from "@forge/ui/out/types/components";
+
+// type ConfigType = {
+//   documentID?: string;
+//   caption?: string;
+//   imageSize?: ImageProps["size"];
+// };
 
 const App = () => {
-  const [data, setData] = useState(null);
+  // const config = (useConfig() || {}) as ConfigType;
+  // const [data, setData] = useState(null);
+  const [productContext, setProductContext] = useState({});
+  // const config = productContext?.extension?.config || ({} as ConfigType);
 
   useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+    invoke("getText", { example: "my-invoke-variable" }).then(setData);
   }, []);
 
   return (
     <>
       <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+      {/* <Text>{data ? data : "Loading..."}</Text> */}
     </>
   );
 };
