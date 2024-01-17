12b9374f72f8abf35e60e1fcba8b5975f0ea30e9
Per Brolin <per@mermaidchart.com>
2024-01-17 09:53:06 +0100

    Added resolver definitions for rest-api functions

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -16,11 +16,42 @@ resolver.define("getText", async (req) => {
   return "Hello, world!!" + projects.length;
 });
 
+resolver.define("getDiagramSVG", async (req) => {
+  console.log(req);
+  const document = req.payload.document;
+  const diagramSVG = await fetchDiagramSVG(document);
+  console.log("diagramSVG: ", diagramSVG);
+  return diagramSVG;
+});
+
+resolver.define("getDocument", async (req) => {
+  console.log(req);
+  const documentID = req.payload.documentID;
+  const document = await fetchDocument(documentID);
+  console.log("document: ", document);
+  return document;
+});
+
+resolver.define("getDocuments", async (req) => {
+  console.log(req);
+  const projectID = req.payload.projectID;
+  const documents = await fetchDocuments(projectID);
+  console.log("documents: ", documents);
+  return documents;
+});
+
 resolver.define("getProjects", async (req) => {
   console.log(req);
   const projects = await fetchProjects();
   console.log("Projects: ", projects);
   return projects;
+});
+
+resolver.define("getTokenExist", async (req) => {
+  console.log(req);
+  const tokenExist = await isTokenExists();
+  console.log("tokenExist: ", tokenExist);
+  return tokenExist;
 });
 
 export const handler = resolver.getDefinitions();
